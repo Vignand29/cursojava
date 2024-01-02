a1488e5d2ae2ea452d2eedeadc9ab8c418a3d0fd
Victor Lopez <vignand29@hotmail.com>
2024-01-02 10:40:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,567 @@
+var datosPersonas = [
+    {
+      Id: 1,
+      Certificacion: "Bussiness Analitycs & Big Data",
+      Curso: "Storeytelling y visualizacion",
+      "Nombre Completo": "Aguilar  Chacon Yuliana",
+      Ced: 115170202
+    },
+    {
+      Id: 2,
+      Certificacion: "Bussiness Analitycs & Big Data",
+      Curso: "Storeytelling y visualizacion",
+      "Nombre Completo": "Castro Montero Tatiana",
+      Ced: 113010860
+    },
+    {
+      Id: 3,
+      Certificacion: "Bussiness Analitycs & Big Data",
+      Curso: "Storeytelling y visualizacion",
+      "Nombre Completo": "Sanchez Sanchez Andres Gonzalo",
+      Ced: 117500766
+    },
+    {
+      Id: 4,
+      Certificacion: "Bussiness Analitycs & Big Data",
+      Curso: "Storeytelling y visualizacion",
+      "Nombre Completo": "Chotto Fernandez Silvia",
+      Ced: 113850480
+    },
+    {
+      Id: 5,
+      Certificacion: "Bussiness Analitycs & Big Data",
+      Curso: "Storeytelling y visualizacion",
+      "Nombre Completo": "Hurtado Hernandez Maria Luz",
+      Ced: 109230064
+    },
+    {
+      Id: 6,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Arias  Asch Diego",
+      Ced: 113700473
+    },
+    {
+      Id: 7,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Castillo Alvarado Javier Alberto",
+      Ced: 2527
+    },
+    {
+      Id: 8,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Guillen Brugman Monica Lizeth",
+      Ced: 160400371306
+    },
+    {
+      Id: 9,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Howell Vargas Lucia del Carmen",
+      Ced: 114960996
+    },
+    {
+      Id: 10,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Mora Araya Gerardo Andres",
+      Ced: 402210002
+    },
+    {
+      Id: 11,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Ortiz Lacayo Sebastian",
+      Ced: 115590057
+    },
+    {
+      Id: 12,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Pertuz Arias Edgard Alexis",
+      Ced: 304840557
+    },
+    {
+      Id: 13,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Prieto Chaparro Rossana Lucia",
+      Ced: 186200328831
+    },
+    {
+      Id: 14,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Hernandez Segura Larissa Maria",
+      Ced: 402190651
+    },
+    {
+      Id: 15,
+      Certificacion: "UI Design",
+      Curso: "Prototipado y Testing",
+      "Nombre Completo": "Montero Solis Fabian",
+      Ced: 111890623
+    },
+    {
+      Id: 16,
+      Certificacion: "UI Design",
+      Curso: "Produccion e Integrables",
+      "Nombre Completo": "Alvarado Barquero Frank Esteban",
+      Ced: 113550233
+    },
+    {
+      Id: 17,
+      Certificacion: "UI Design",
+      Curso: "Produccion e Integrables",
+      "Nombre Completo": "Porras Morales Miguel Alberto",
+      Ced: 113110735
+    },
+    {
+      Id: 18,
+      Certificacion: "UI Design",
+      Curso: "Produccion e Integrables",
+      "Nombre Completo": "Ramirez Rojas Priscilla",
+      Ced: 114700049
+    },
+    {
+      Id: 19,
+      Certificacion: "UI Design",
+      Curso: "Produccion e Integrables",
+      "Nombre Completo": "Sanchez Leon Melissa Elena",
+      Ced: 402260139
+    },
+    {
+      Id: 20,
+      Certificacion: "UI Design",
+      Curso: "Produccion e Integrables",
+      "Nombre Completo": "Viquez Valerio  Adriana",
+      Ced: 114960286
+    },
+    {
+      Id: 21,
+      Certificacion: "UI Design",
+      Curso: "Produccion e Integrables",
+      "Nombre Completo": "Zunniga Arias Miguel",
+      Ced: 112180804
+    },
+    {
+      Id: 22,
+      Certificacion: "UX Design",
+      Curso: "Principios del disenno, interaccion y usabilidad",
+      "Nombre Completo": "Calzada Lang Maria Alejandra",
+      Ced: 114630183
+    },
+    {
+      Id: 23,
+      Certificacion: "UX Design",
+      Curso: "Principios del disenno, interaccion y usabilidad",
+      "Nombre Completo": "Chang Rodriguez Rita Irene",
+      Ced: 117490968
+    },
+    {
+      Id: 24,
+      Certificacion: "UX Design",
+      Curso: "Principios del disenno, interaccion y usabilidad",
+      "Nombre Completo": "Gonzalez Pereira Priscila",
+      Ced: 402130019
+    },
+    {
+      Id: 25,
+      Certificacion: "UX Design",
+      Curso: "Principios del disenno, interaccion y usabilidad",
+      "Nombre Completo": "Ovares Granados Karol Viviana",
+      Ced: 114570276
+    },
+    {
+      Id: 26,
+      Certificacion: "UX Design",
+      Curso: "Principios del disenno, interaccion y usabilidad",
+      "Nombre Completo": "Saborio Hernandez Jorge Esteban",
+      Ced: 113280785
+    },
+    {
+      Id: 27,
+      Certificacion: "UX Design",
+      Curso: "Planificacion y elaboracion del proyecto",
+      "Nombre Completo": "Chacon Jimenez Krisia",
+      Ced: 115180868
+    },
+    {
+      Id: 28,
+      Certificacion: "UX Design",
+      Curso: "Planificacion y elaboracion del proyecto",
+      "Nombre Completo": "Diaz Quiros Mariana Guadalupe",
+      Ced: 207280327
+    },
+    {
+      Id: 29,
+      Certificacion: "UX Design",
+      Curso: "Planificacion y elaboracion del proyecto",
+      "Nombre Completo": "Herrera  Garcia Gabriela",
+      Ced: 114870325
+    },
+    {
+      Id: 30,
+      Certificacion: "UX Design",
+      Curso: "Planificacion y elaboracion del proyecto",
+      "Nombre Completo": "Jimenez Quijano Alejandro",
+      Ced: 111040561
+    },
+    {
+      Id: 31,
+      Certificacion: "UX Design",
+      Curso: "Planificacion y elaboracion del proyecto",
+      "Nombre Completo": "Palma Jimenez Liss Francela",
+      Ced: 115970843
+    },
+    {
+      Id: 32,
+      Certificacion: "UX Design",
+      Curso: "Planificacion y elaboracion del proyecto",
+      "Nombre Completo": "Rios Valverde Angelica Maria",
+      Ced: 116730717
+    },
+    {
+      Id: 33,
+      Certificacion: "UX Design",
+      Curso: "Planificacion y elaboracion del proyecto",
+      "Nombre Completo": "Sanabria  Barboza  Jonathan de los Ángeles",
+      Ced: 116190975
+    },
+    {
+      Id: 34,
+      Certificacion: "UX Design",
+      Curso: "Planificacion y elaboracion del proyecto",
+      "Nombre Completo": "Villegas  Cespedes Kattia",
+      Ced: 402030445
+    },
+    {
+      Id: 35,
+      Certificacion: "UX Design",
+      Curso: "Planificacion y elaboracion del proyecto",
+      "Nombre Completo": "Wind Mora Laura",
+      Ced: 116520848
+    },
+    {
+      Id: 36,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "Barrantes Moya Danny Gabriel",
+      Ced: 207170732
+    },
+    {
+      Id: 37,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "Quesada Elizondo Kevin",
+      Ced: 207700811
+    },
+    {
+      Id: 38,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "Salazar Gil Juan Jose",
+      Ced: 116410891
+    },
+    {
+      Id: 39,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "Brenes Gutierrez Andres",
+      Ced: 112460470
+    },
+    {
+      Id: 40,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "Calderon Calderon Carlos Eduardo",
+      Ced: 112490873
+    },
+    {
+      Id: 41,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "de la Ossa Picado Malena",
+      Ced: 115980879
+    },
+    {
+      Id: 42,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "Ibarra Morera Maria Fernanda",
+      Ced: 206900233
+    },
+    {
+      Id: 43,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "Paniagua Sanchez Kevin",
+      Ced: 113490874
+    },
+    {
+      Id: 44,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "Sequeira Cordero Jose Pablo",
+      Ced: 702330791
+    },
+    {
+      Id: 45,
+      Certificacion: "Front End Development",
+      Curso: "Java Script From Zero to Hero",
+      "Nombre Completo": "Vargas  Molina Ricardo Alonso",
+      Ced: 114460601
+    },
+    {
+      Id: 46,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Arias Jimenez Daniela",
+      Ced: 117730722
+    },
+    {
+      Id: 47,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Arias Mora Alonso Alberto",
+      Ced: 4821
+    },
+    {
+      Id: 48,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Jenkins Duran Hernan Josue",
+      Ced: 503950664
+    },
+    {
+      Id: 49,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Lapeira Mix Ileana",
+      Ced: 4552
+    },
+    {
+      Id: 50,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Soto Gonzalez Alejandro",
+      Ced: 503630233
+    },
+    {
+      Id: 51,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Sredni Yaker Michelle",
+      Ced: 117000218112
+    },
+    {
+      Id: 52,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Rojas  Porras Javier",
+      Ced: 604110783
+    },
+    {
+      Id: 53,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Sanchez Aviles Ariel",
+      Ced: 702140333
+    },
+    {
+      Id: 54,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Sandi Ruiz Maria Fernanda",
+      Ced: 117230395
+    },
+    {
+      Id: 55,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Aguilar Moraga Katherine Natalia",
+      Ced: 113820805
+    },
+    {
+      Id: 56,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Álvarez Calderon Laura",
+      Ced: 206160917
+    },
+    {
+      Id: 57,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Arias Asch Mariana",
+      Ced: 114590186
+    },
+    {
+      Id: 58,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Cerdas Chavarria Marian",
+      Ced: 206940666
+    },
+    {
+      Id: 59,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Ferreto Sanchez Andrea",
+      Ced: 303960078
+    },
+    {
+      Id: 60,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Gutierrez Guillen Maria Fernanda",
+      Ced: 115750422
+    },
+    {
+      Id: 61,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Marin Chaves Maria Pamela",
+      Ced: 207220204
+    },
+    {
+      Id: 62,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Mora Torres Yuliana",
+      Ced: 207250598
+    },
+    {
+      Id: 63,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Vargas Oliva Maria Alejandra",
+      Ced: 115240960
+    },
+    {
+      Id: 64,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Villegas Rodriguez Cristhian Danilo",
+      Ced: 503920384
+    },
+    {
+      Id: 65,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Arce Castillo Andrea",
+      Ced: 115350910
+    },
+    {
+      Id: 66,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Arias Fonseca Maria Fernanda",
+      Ced: 116240420
+    },
+    {
+      Id: 67,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Diaz Esquivel Jason",
+      Ced: 113830963
+    },
+    {
+      Id: 68,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Gutierrez Herrera Bayron Roberto",
+      Ced: 116250695
+    },
+    {
+      Id: 69,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Mendez Cedenno Victor Manuel",
+      Ced: 116180190
+    },
+    {
+      Id: 70,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Ostorga  Chacon  Erick Leonidas",
+      Ced: 304710206
+    },
+    {
+      Id: 71,
+      Certificacion: "UX Design",
+      Curso: "Investigacion para el Disenno de Experiencias Digitales",
+      "Nombre Completo": "Solano Abarca Andres",
+      Ced: 207240493
+    },
+    {
+      Id: 72,
+      Certificacion: "UI Design",
+      Curso: "Disenno de Interfaces",
+      "Nombre Completo": "Aguilar Calvo Juan Eduardo",
+      Ced: 115700251
+    },
+    {
+      Id: 73,
+      Certificacion: "UI Design",
+      Curso: "Disenno de Interfaces",
+      "Nombre Completo": "Aguirre  Arce  Harold",
+      Ced: 110580394
+    },
+    {
+      Id: 74,
+      Certificacion: "UI Design",
+      Curso: "Disenno de Interfaces",
+      "Nombre Completo": "Cardenas Carmona Gabriela",
+      Ced: 112420515
+    },
+    {
+      Id: 75,
+      Certificacion: "UI Design",
+      Curso: "Disenno de Interfaces",
+      "Nombre Completo": "Garro Saborio Randall David",
+      Ced: 115630108
+    },
+    {
+      Id: 76,
+      Certificacion: "UI Design",
+      Curso: "Disenno de Interfaces",
+      "Nombre Completo": "Obando Alvarado Jose Andres",
+      Ced: 304970368
+    },
+    {
+      Id: 77,
+      Certificacion: "UI Design",
+      Curso: "Disenno de Interfaces",
+      "Nombre Completo": "Prado Munnoz Thelma Vanessa",
+      Ced: 112870529
+    },
+    {
+      Id: 78,
+      Certificacion: "UI Design",
+      Curso: "Disenno de Interfaces",
+      "Nombre Completo": "Villela Herrera Maria",
+      Ced: 114380008
+    }
+  ];
+  
+  function Imprimir(Id) {
+    var x = Id;
+    document.getElementById("txt_Id").value = datosPersonas[x]["Id"];
+    document.getElementById("txt_Certificacion").value =
+      datosPersonas[x]["Certificacion"];
+    document.getElementById("txt_Curso").value = datosPersonas[x]["Curso"];
+    document.getElementById("txt_Nombre").value =
+      datosPersonas[x]["Nombre Completo"];
+    document.getElementById("txt_Cedula").value = datosPersonas[x]["Ced"];
+  }
+  
+  function Limpiar() {
+    document.getElementById("txt_Id").value = "";
+    document.getElementById("txt_Certificacion").value = "";
+    document.getElementById("txt_Curso").value = "";
+    document.getElementById("txt_Nombre").value = "";
+    document.getElementById("txt_Cedula").value = "";
+  }
